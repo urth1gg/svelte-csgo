cde54567a1fced0a6c57bde739b21355cfce2c4a
Djordje Jevremovic <djolecs97@gmailcom>
2022-11-16 09:52:29 +0100

    Latest update

--- src/lib/components/modals/PartyModal.svelte
+++ src/lib/components/modals/PartyModal.svelte
@@ -2,18 +2,51 @@
     import { userData } from '$lib/store/userData';
     import { modals } from '$lib/store/modals';
     import UserDisplay from '$lib/components/user/User.svelte';
+	import { FriendRequestStatus } from '$lib/enums/enums';
 
     let friends: Friend[]  = [];
 
-    userData.subscribe( value => {
-        friends = value.friends as Friend[];
-    })
+    // populate friends with fake data 
+    for(let i = 0; i < 10; i++){
+        friends.push({
+            id: i.toString(),
+            user:{
+                id: i.toString(),
+                username: 'test',
+                profile_img: '',
+                created_at: '',
+                email: '',
+                isFriend: null,
+                stats: [{
+                    elo: 1000,
+                    wins: 0,
+                    losses: 0,
+                    games_played: 0,
+                    user_id: i.toString(),
+                    kills: 0,
+                    deaths: 0,
+                    created_at: '',
+                }],
+            },
+            friend: {
+                user_id: i.toString(),
+                friend_id: i.toString(),
+                created_at: '',
+                status: FriendRequestStatus.ACCEPTED,
+            }
+        })
+    }
+
+
+    // userData.subscribe( value => {
+    //     friends = value.friends as Friend[];
+    // })
 </script>  
 
 {#if $modals.showParty}
 
 <div class="overlay">
-        <div class="w-[300px] flex flex-col gap-2 modal-friends section2">
+        <div class="w-[300px] flex flex-col modal-friends section2">
             {#each friends as friend}
                 <UserDisplay user={friend.friend} clickEffect='INVITE_TO_PARTY' />
             {/each}
--- src/lib/components/user/User.svelte
+++ src/lib/components/user/User.svelte
@@ -6,26 +6,30 @@
     function clickHandler(){
         console.log('test')
     }
+    console.log(clickEffect)
 </script>
 
 {#if clickEffect === 'INVITE_TO_PARTY'}
-    <button data-sveltekit-reload class="flex items-center p-2 border-t border-gray-200 user" on:click={clickHandler}>
+    <button data-sveltekit-reload class="flex items-center p-2 user" on:click={clickHandler}>
         {#if user.profile_img}<img src={user.profile_img} alt={user.username} class="w-8 h-8 rounded-full"/>
         {:else}
             <div class="w-8 h-8 rounded-full bg-[var(--test-color)] flex justify-center items-center">
                 <i class="fas fa-user text-sm text-white"></i>
             </div>
         {/if}
-        <div class="ml-2">
-            <a data-sveltekit-reload href="/profile/{user.username}">
+        <div class="ml-2 w-full justify-between flex items-center user-border">
+            <div class="">
                 <p class="text-sm font-bold">
                     {#if user.flags?.[0].is_online} 
                         <i class="fas fa-circle text-green-500 text-xs"></i>
                     {/if}
                     {user.username}
+                    
                 </p>
-            </a>
-            <p class="text-xs text-white">ELO: {user.stats?.[0].elo}</p>
+                <p class="text-xs text-white">ELO: {user.stats?.[0].elo}</p>
+
+            </div>
+            <p class="text-white">Click to invite</p>
         </div>
     </button>
 {:else}
